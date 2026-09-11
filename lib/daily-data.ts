4c149d6ea98ae1e45aeb85b0f2127aee27aad6a1
triple cognitive <1214636153@qq.com>
2026-09-11 22:25:30 +0800

    publish GitHub daily brief for 2026-09-11

--- lib/daily-data.ts
+++ lib/daily-data.ts
@@ -1,5 +1,5 @@
 import type { ContentRecord, DailyBriefPayload } from './content-model';
-import dailyBriefJson from '@/content/daily/2026-09-10.json';
+import dailyBriefJson from '@/content/daily/2026-09-11.json';
 
 export const dailyBrief = dailyBriefJson as ContentRecord<DailyBriefPayload>;
 
